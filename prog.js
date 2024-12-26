5d019a28d52c3d044d1d93460a5b6ce37c112241
Anonuevo <geroanonuevo3@gmail.com>
2024-12-26 09:46:54 +0800

    finished

---
+++ prog.js
@@ -0,0 +1,12 @@
+var firstNumber = 55
+var secondNumber = 77
+
+function sumOrProduct(first, second){
+    if(first*second<=1000){
+    console.log(first*second)
+}else{
+    console.log(first+second)
+};
+}
+
+sumOrProduct(firstNumber, secondNumber)
